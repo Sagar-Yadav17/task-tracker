85ee61e6bd2fb6597b5ad34f0236465a88898d2d
Sagar Yadav <sagarslife222@gmail.com>
2026-05-02 19:34:56 +0530

    change base url saved

--- frontend/src/api/api.js
+++ frontend/src/api/api.js
@@ -1,8 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  //baseURL: "https://task-tracker-b-nwhp.onrender.com",
-  baseURL: "http://localhost:5000",
+  baseURL: "https://task-tracker-b-nwhp.onrender.com"
 });
 
 // Attach token automatically
